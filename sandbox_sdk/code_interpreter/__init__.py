213eab3e185b4377674ef4788693a902c56b9417
huang-jl <1046678590@qq.com>
2025-02-23 16:04:13 +0800

    Add support for interrupt jupyter kernel

--- sandbox_sdk/code_interpreter/__init__.py
+++ sandbox_sdk/code_interpreter/__init__.py
@@ -1,4 +1,4 @@
 from sandbox_sdk import *
 
-from .main import CodeInterpreter, JupyterExtension
+from .main import CodeInterpreter, JupyterExtension, JupyterKernelWebSocket
 from .models import Execution, Error, Result, KernelException, MIMEType, Logs
